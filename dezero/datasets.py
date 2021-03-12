c185af33eb64391627cf84832c3c891a04a6bedc
Leejunho <73324807+Leejunho123@users.noreply.github.com>
2021-03-12 16:15:14 +0900

    Update datasets.py

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -82,7 +82,7 @@ class MNIST(Dataset):
         super().__init__(train, transform, target_transform)
 
     def prepare(self):
-        url = 'https://yann.lecun.com/exdb/mnist/'
+        url = 'http://yann.lecun.com/exdb/mnist/'
         train_files = {'target': 'train-images-idx3-ubyte.gz',
                        'label': 'train-labels-idx1-ubyte.gz'}
         test_files = {'target': 't10k-images-idx3-ubyte.gz',
